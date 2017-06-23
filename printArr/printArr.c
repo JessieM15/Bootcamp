cc93d910902996368573b419fa2ccd8a3c84a295
admin <admin@localhost.localdomain>
2017-06-22 17:57:00 -0700

    printarr

---
+++ printArr/printArr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// printArray goes here
+void printArray(int a[], int size){
+  int i;
+  for (i = 0; i < size; i++) {
+    /* code */
+    printf("%d\n", a[i]);
+  }
+}
+
+int main() {
+  int arraySize = 14;
+  int myArray[arraySize];
+  int i;
+  myArray[0] = 0;
+  myArray[1] = 1;
+  for (i = 2; i < arraySize; i++) {
+  	myArray[i] = myArray[i-2] + myArray[i-1];
+  }
+  printArray(myArray, arraySize);
+  return 0;
+}
